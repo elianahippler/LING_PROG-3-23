b759422e80433f756b2a421396a971f8822b6287
Eliana Hippler <elianahippler@gmail.com>
2023-02-22 21:12:12 -0300

    Add files via upload

---
+++ 3cti-eliana/1_intro/3cti-eliana/1_intro/2_variaveis.php
@@ -0,0 +1,25 @@
+<?php
+    $name = "Guilherme";
+    $age = 20;
+
+    echo $name; 
+    echo "</br>";
+    echo $age; 
+    echo "<hr>";
+
+    $b = true; /*boolean*/
+    echo gettype($b);
+    echo "<hr>";
+
+    $nasc = 1994; /*integer*/
+    echo gettype($nasc);
+    echo "<hr>";
+    
+    $nome = "Eliana"; /*string*/
+    echo gettype($nome);
+    echo "<hr>";
+
+    $num = 27.09; /*float|double*/
+    echo gettype($num);
+    echo "<hr>";
+?>
